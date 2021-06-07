c0d2596e05b907be8efafe97186851298e092498
ullasmshetty <83344617+ullasmshetty@users.noreply.github.com>
2021-06-07 07:48:56 +0530

    Update script.js

--- Module_4 Solution/script.js
+++ Module_4 Solution/script.js
@@ -10,11 +10,11 @@ names[7]="paula";
 names[8]="laura";
 names[9]="jim";
 
-for (var i=0; i < names.length; i++){
-   if(names[i].charAt(0)==='J' || names[i].charAt(0)==='j) {
-      console.log("Goodbye "+names[i])
-}
-else {
-   console.log("Hello " +names[i])
-}
-}
+for (var i=0 ; i<names.length;i++){
+    if(names[i].charAt(0)==='J' || names[i].charAt(0)==='j'){
+        console.log("Goodbye "+names[i])
+    }
+    else{
+        console.log("Hello "+names[i])
+    }
+    }
